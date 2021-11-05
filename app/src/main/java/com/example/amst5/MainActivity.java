b166993459fbbdfd043034a63ef2d39ce847241a
joistaus <joistara@espol.edu.ec>
2021-11-04 23:13:04 -0500

    [MOD] Linking other activities to MainActivity

--- app/src/main/java/com/example/amst5/MainActivity.java
+++ app/src/main/java/com/example/amst5/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.amst5;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToVideo(View view) {
+        Intent intent = new Intent(
+                this,
+                VideoView.class
+        );
+        startActivity(intent);
+    }
+
+    public void goToCalendar(View view) {
+
+    }
+
+    public void goToMap(View view) {
+
+    }
+
+    public void goToGraphic(View view) {
+
     }
 }
